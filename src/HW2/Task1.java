52fddd35d075ea285acb518b62d3c5f2b6e5ba2e
verytallman <oleksiidehtiarov@gmail.com>
2021-11-09 10:03:23 +0200

    task 1-3 start

--- src/HW2/Task1.java
+++ src/HW2/Task1.java
@@ -4,11 +4,19 @@ package HW2;
 //больше или равно 50 или вывести “FAIL” в противном случае.
 //Программа всегда должна перед выходом печатать “DONE”
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-
-
+        System.out.println("This is CheckPassFail program!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ENTER YOUR INTEGER:");
+        int mark = scanner.nextInt();
+        if (mark >= 50) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+        System.out.println("DONE");
     }
 }
--- src/HW2/Task2.java
+++ src/HW2/Task2.java
@@ -4,8 +4,19 @@ package HW2;
 // если переменная “number” типа int нечетная.
 // Или “Even Number” если переменная четная. Программа всегда должна перед выходом печатать “BYE”
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        System.out.println("This is CheckOddEven program!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ENTER YOUR INTEGER:");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+        System.out.println("BYE");
     }
 }
