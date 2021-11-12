a12302ff7574e9bb50b02fb07350976a9b37c014
verytallman <oleksiidehtiarov@gmail.com>
2021-11-12 10:58:00 +0200

    Equation

--- src/HW2/Task5.java
+++ src/HW2/Task5.java
@@ -9,17 +9,15 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String[] args) {
         System.out.println("This is equation solver!");
-        System.out.println("Equation looks like a*x+b=c");
-        System.out.println("In my case equation is 5 * x + 25 = 50");
+        System.out.println("Equation looks like a*x+b=0");
         Scanner scanner1 = new Scanner(System.in);
         System.out.print("Type here value for a:");
         int a = scanner1.nextInt();
         Scanner scanner2 = new Scanner(System.in);
         System.out.print("Type here value for b:");
         int b = scanner2.nextInt();
-        int c = 50; //ответ после знака "="
         if (a > 0 && b >0){
-            int x = (c - b) / a;
+            int x = (-b / a);
             System.out.println("Икс равен: "+x);
         }else if (a == 0 || b == 0){
             System.out.println("Одна из цифр равна нулю.");
---
+++ src/Lesson3/lesson3Main.java
@@ -0,0 +1,31 @@
+package Lesson3;
+
+public class lesson3Main {
+
+//        public static void main(String args[]){    //Улучшенный цикл for
+//            int [] numbers = {10, 20, 30, 40, 50};
+//
+//            for(int x : numbers ){                 //Объявление: к новому объявленному блоку переменной осуществляется доступ, который имеет тип совместимый с элементами массива.
+//                                                      Выражение: вычисляет нужное Вам в массиве цикла. Выражение может быть переменной или методом массива, вызов которого возвращает массив.
+//                System.out.print( x );
+//                System.out.print(",");
+//            }
+//            System.out.print("\n");
+//            String [] names ={"Олег", "Иван", "Дима", "Юля"};
+//            for( String name : names ) {
+//                System.out.print( name );
+//                System.out.print(",");
+//            }
+//        }
+//    }
+//public static void main(String[] args) {        /while
+//    int x = 10;
+//
+//    while( x < 16 ) {
+//        System.out.print("Значение x: " + x );
+//        x++;
+//        System.out.print("\n");
+//    }
+//}
+
+}
