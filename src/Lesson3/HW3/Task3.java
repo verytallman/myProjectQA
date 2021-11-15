7aad8f930b833af9e59724980d2f657598c9b22f
verytallman <oleksiidehtiarov@gmail.com>
2021-11-15 10:27:14 +0200

    task3+4

--- src/Lesson3/HW3/Task3.java
+++ src/Lesson3/HW3/Task3.java
@@ -2,5 +2,12 @@ package Lesson3.HW3;
 
 //Write a program that finds the minimum value in an array
 
+import java.util.Arrays;
+
 public class Task3 {
+    public static void main(String[] args) {
+        int[] nums = {6128354, 12612, 1231, 5125125, 1251, 124316, 1744, 1488, 152151};
+        Arrays.sort(nums);
+        System.out.println("Minimum value in an array " + nums[0]);
+    }
 }
