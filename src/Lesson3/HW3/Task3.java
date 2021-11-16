0ce3155156f4ae12247ab2d34267cc34ccca420b
verytallman <oleksiidehtiarov@gmail.com>
2021-11-16 17:16:51 +0200

    Task3HW3

--- src/Lesson3/HW3/Task3.java
+++ src/Lesson3/HW3/Task3.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 
 public class Task3 {
     public static void main(String[] args) {
-        int[] nums = {6128354, 12612, 1231, 5125125, 1251, 124316, 1744, 1488, 152151};
-        Arrays.sort(nums);
-        System.out.println("Minimum value in an array " + nums[0]);
+        int[] arr = {6128354, 12612, 1231, 5125125, 1251, 124316, 1744, 1488, 152151};
+        Arrays.sort(arr);
+        System.out.println("Minimum value in an array " + arr[0]);
     }
 }
