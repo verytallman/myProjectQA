2aa3089a9598c0ea739847c3c70f19a1bcc41033
verytallman <oleksiidehtiarov@gmail.com>
2021-11-17 20:39:30 +0200

    Task2HW4

--- src/Lesson4/HW4/Task2.java
+++ src/Lesson4/HW4/Task2.java
@@ -4,6 +4,16 @@ package Lesson4.HW4;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println("The sum of a given array is: " + sumOfArray());
 
+    }
+
+    public static int sumOfArray() {
+        int [] arr = {1,12,612,1488};
+        int sumOfArray = 0;
+        for (int i = 0; i < arr.length; i++){
+            sumOfArray = sumOfArray + arr[i];
+        }
+        return sumOfArray;
     }
 }
