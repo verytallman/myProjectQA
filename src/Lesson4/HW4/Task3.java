b218d019e3262c7f42b071c79a7490b16d43e45b
verytallman <oleksiidehtiarov@gmail.com>
2021-11-19 10:12:09 +0200

    Task3

--- src/Lesson4/HW4/Task3.java
+++ src/Lesson4/HW4/Task3.java
@@ -2,8 +2,26 @@ package Lesson4.HW4;
 //Write a method which returns an array of
 //codes for given array of chars
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        System.out.println("Please, enter your char's: ");
+        System.out.println(codeOfChar());
 
+    }
+
+    public static String codeOfChar (){
+        int i;
+        Scanner scanner = new Scanner(System.in);
+        String charsEntered = scanner.nextLine();
+        char [] arr = charsEntered.toCharArray();
+        int numberOfChars = charsEntered.length();
+        for (i = 0; i < numberOfChars; i++){
+            char character = charsEntered.charAt(i);
+            int ascii = character;
+            System.out.println("The ASCII code for "+"'"+character+"'" +" is"+ " "+ascii+".");
+        }
+        return "";
     }
 }
