d7e19b26958ddcc2bdbad4594826d8f912fa7f62
verytallman <oleksiidehtiariov@gmail.com>
2021-11-22 12:26:18 +0200

    task1HW6

--- src/Lesson5/HW5/Task1.java
+++ src/Lesson5/HW5/Task1.java
@@ -5,5 +5,29 @@ package Lesson5.HW5;
 //sentence) to upper and the rest to lower, etc.
 
 
+
+import javax.sound.midi.Soundbank;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println(allUpperCaseConverter());
+    }
+
+    public static String allUpperCaseConverter() {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        int i;
+        StringBuffer outputString = new StringBuffer(inputString);
+        for (i = 0; i < inputString.length(); i++) {
+            if (Character.isLowerCase(inputString.charAt(i))){
+                outputString.setCharAt(i,Character.toUpperCase(inputString.charAt(i)));
+            } else if (Character.isUpperCase(inputString.charAt(i))){
+                outputString.setCharAt(i,Character.toLowerCase(inputString.charAt(i)));
+            }
+        }
+        System.out.println("Converted string is: "+outputString);
+        return "";
+    }
 }
