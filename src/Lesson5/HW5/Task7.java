b01b5835f578b62f5d31270a2c578dc042b15a22
verytallman <oleksiidehtiariov@gmail.com>
2021-11-24 11:40:57 +0200

    start hw6

--- src/Lesson5/HW5/Task7.java
+++ src/Lesson5/HW5/Task7.java
@@ -8,4 +8,19 @@ package Lesson5.HW5;
 //phel
 
 public class Task7 {
+    public static void main(String[] args) {
+        String str = new String("Старлей");
+        squareMaker(str);
+    }
+
+    public static void squareMaker (String str) {
+        int wordLength = str.length();
+        StringBuffer str2 = new StringBuffer(str);
+        str2.append(str);
+        for (int i = 0; i < wordLength; i++) {
+            for (int j = 0; j != wordLength; j++)
+                System.out.print(str2.charAt(i + j));
+            System.out.println();
+        }
+    }
 }
