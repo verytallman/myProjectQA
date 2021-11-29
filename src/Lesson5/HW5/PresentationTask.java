2c2f3b9347269cc4be2bc04352e3c143772c337c
verytallman <oleksiidehtiarov@gmail.com>
2021-11-29 18:58:12 +0200

    HW

--- src/Lesson5/HW5/PresentationTask.java
+++ src/Lesson5/HW5/PresentationTask.java
@@ -10,11 +10,58 @@ import java.util.Scanner;
 
 public class PresentationTask {
     public static void main(String[] args) {
-    }
-
-    public static String randomStringGenerator (String str1, int length){
+        int n = 20;
+        System.out.println("1. Numeric");
+        int a = 1;
+        System.out.println("2. Alpha");
+        int b = 2;
+        System.out.println("3. AlphaNumeric");
+        int c = 3;
+        System.out.print("Please, enter desirable result:");
         Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
 
-return "" ;
+
+        if (input == a){
+            System.out.println(getNumeric(n));
+        }else if (input == b){
+            System.out.println(getAlphaString(n));
+        }else if (input == c){
+            System.out.println(getAlphaNumericString(n));
+        }
     }
+
+
+    static String getNumeric (int n) {
+        String Numeric = "0123456789";
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = (int) (Numeric.length() * Math.random());
+            sb.append(Numeric.charAt(index));
+        }return sb.toString();
+    }
+
+    static String getAlphaString(int n) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = (int) (AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }return sb.toString();
+    }
+
+    static String getAlphaNumericString(int n) {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
 }
