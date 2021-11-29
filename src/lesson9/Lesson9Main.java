9d2d179537d6c2438fb2702995adc3d661b5e88c
verytallman <oleksiidehtiarov@gmail.com>
2021-11-29 19:49:45 +0200

    l9

--- src/lesson9/Lesson9Main.java
+++ src/lesson9/Lesson9Main.java
@@ -4,6 +4,8 @@ public class Lesson9Main {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("123");
+
+        System.out.println("1234165");
     }
 
 }
