996b42c91b9c41e6c6c957684bc7be0b8506a8bb
verytallman <oleksiidehtiarov@gmail.com>
2021-11-29 19:08:24 +0200

    HW

--- src/Lesson5/HW5/Task6_ProductCodeValidation.java
+++ src/Lesson5/HW5/Task6_ProductCodeValidation.java
@@ -9,42 +9,30 @@ public class Task6_ProductCodeValidation {
     public static void main(String[] args) {
         System.out.println(productCodeValidator("AX6BYU56UX6CV6BNT7NM 287430"));
     }
-    public static String productCodeValidator (String input) {
+
+    public static String productCodeValidator(String input) {
         String digits = input.replaceAll("[A-Z]", "");
-        char [] parsedIntsInArray = digits.toCharArray();
-        String firsInt = Character.toString(parsedIntsInArray[0])+Character.toString(parsedIntsInArray[1]);
-        String secondInt = Character.toString(parsedIntsInArray[2])+Character.toString(parsedIntsInArray[3]);
-        String thirdInt = Character.toString(parsedIntsInArray[4])+Character.toString(parsedIntsInArray[5]);
 
-        String result = Character.toString(parsedIntsInArray[7])+Character.toString(parsedIntsInArray[8])+
-               Character.toString(parsedIntsInArray[9])+Character.toString(parsedIntsInArray[10])+
-               Character.toString(parsedIntsInArray[11])+Character.toString(parsedIntsInArray[12]);
+        char[] parsedIntsInArray = digits.toCharArray();
 
-        int a = Integer.parseInt(firsInt)*Integer.parseInt(secondInt)*Integer.parseInt(thirdInt);
+        String firsInt = Character.toString(parsedIntsInArray[0]) + Character.toString(parsedIntsInArray[1]);
+
+        String secondInt = Character.toString(parsedIntsInArray[2]) + Character.toString(parsedIntsInArray[3]);
+
+        String thirdInt = Character.toString(parsedIntsInArray[4]) + Character.toString(parsedIntsInArray[5]);
+
+        String result = Character.toString(parsedIntsInArray[7]) + Character.toString(parsedIntsInArray[8]) +
+                        Character.toString(parsedIntsInArray[9]) + Character.toString(parsedIntsInArray[10]) +
+                        Character.toString(parsedIntsInArray[11]) + Character.toString(parsedIntsInArray[12]);
+
+        int a = Integer.parseInt(firsInt) * Integer.parseInt(secondInt) * Integer.parseInt(thirdInt);
         int b = Integer.parseInt(result);
 
-        if( a == b ) {
+        if (a == b) {
             System.out.println("Correct");
-        }else {
+        } else {
             System.out.println("Incorrect");
         }
-
-
-
-
-
-
-//        String number = "";
-//        int i;
-//        for (i = 0; i < input.length(); i++) {
-//            char [] arrayOfDigits = number.toCharArray();
-//            char a = input.charAt(i);
-//            if (Character.isDigit(a)){
-//                number = number + a;
-//            }
-//          //  System.out.println(qwe);
-//        } return number;
-//   int[] qwe = Integer.parseInt(number);
-    return "";
+        return "";
     }
 }
